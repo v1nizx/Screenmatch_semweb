a8f85b690a636aa3bcaaee975929d71968c6e99d
Marcos Vinicius <144844828+v1nizx@users.noreply.github.com>
2024-07-15 11:41:28 -0300

    Add files via upload

--- src/main/java/br/com/alura/Screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/Screenmatch/principal/Principal.java
@@ -6,8 +6,11 @@ import br.com.alura.Screenmatch.services.ConsumoApi;
 import br.com.alura.Screenmatch.services.ConverterDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
 
@@ -44,5 +47,16 @@ public class Principal {
 //        }
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadoEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episodios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadoEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
